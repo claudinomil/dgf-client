05aa3cbd97c206ca55e800c1f9bc328d87c599b7
claudinomil <claudinomoraes@yahoo.com.br>
2024-03-07 09:50:12 -0300

    Atualização 6

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -3,6 +3,8 @@
 <!-- esses link's abaixo serão utilizados na customização vinda do arquivo app.min.js -->
 
 <!-- Bootstrap Css -->
-<link href="{{asset('build/assets/bootstrap.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+{{--<link href="{{asset('build/assets/bootstrap.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />--}}
+<link href="{{asset('build/assets/bootstrap.css')}}" id="bootstrap-style" type="text/css" />
 <!-- App Css-->
-<link href="{{asset('build/assets/app.css')}}" id="app-style" rel="stylesheet" type="text/css" />
+{{--<link href="{{asset('build/assets/app.css')}}" id="app-style" rel="stylesheet" type="text/css" />--}}
+<link href="{{asset('build/assets/app.css')}}" id="app-style" type="text/css" />
