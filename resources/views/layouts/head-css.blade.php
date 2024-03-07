63e8b9e85ed11e3005f9868011e0d72d60004d3f
claudinomil <claudinomoraes@yahoo.com.br>
2024-03-07 09:55:17 -0300

    Atualização 7

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -5,6 +5,7 @@
 <!-- Bootstrap Css -->
 {{--<link href="{{asset('build/assets/bootstrap.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />--}}
 <link href="{{asset('build/assets/bootstrap.css')}}" id="bootstrap-style" type="text/css" />
+
 <!-- App Css-->
 {{--<link href="{{asset('build/assets/app.css')}}" id="app-style" rel="stylesheet" type="text/css" />--}}
 <link href="{{asset('build/assets/app.css')}}" id="app-style" type="text/css" />
