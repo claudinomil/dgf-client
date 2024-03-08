11ed91319028d259c9b9b6a6713f6b3fa111f348
claudinomil <claudinomoraes@yahoo.com.br>
2024-03-08 10:38:24 -0300

    Atualização CSS 3

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -9,4 +9,4 @@
 <link href="{{asset('build/assets/app.css')}}" id="app-style" rel="stylesheet" type="text/css" />
 
 <!-- Icons Css -->
-{{--<link href="{{asset('build/assets/icons.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />--}}
+<link href="{{asset('build/assets/icons.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
